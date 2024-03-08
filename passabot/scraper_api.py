4d5427956b2ebf7635165d455e462be4cedaaf21
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-03-08 11:54:52 +0100

    feat: use requests session

--- passabot/scraper_api.py
+++ passabot/scraper_api.py
@@ -2,7 +2,7 @@
 
 import asyncio
 import logging
-from typing import Any, NoReturn
+from typing import NoReturn
 
 import requests
 from selenium.common.exceptions import NoSuchElementException
@@ -23,9 +23,13 @@ class ResponseError(Exception):
 
 
 class ApiScraper(IScraper):
+    USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
+
     def __init__(self, authenticator: IAuthenticator, province: str) -> None:
         self.authenticator = authenticator
         self.province = province
+        self.session = requests.Session()
+        self.session.headers.update({"User-Agent": self.USER_AGENT})
 
     async def login(self) -> bool:
         try:
@@ -33,27 +37,13 @@ class ApiScraper(IScraper):
         except NoSuchElementException:
             return False
 
-        self.csrf_token = auth_data.csrf_token
-        self.session_id = auth_data.session_id
+        self.session.cookies.set("JSESSIONID", auth_data.session_id)
+        self.session.headers.update({"X-Csrf-Token": auth_data.csrf_token})
+
         return True
 
-    def _post(self, endpoint: str, json: dict[str, Any]) -> requests.Response:
-        USER_AGENT = (
-            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
-        )
-
-        return requests.post(
-            endpoint,
-            json=json,
-            headers={
-                "User-Agent": USER_AGENT,
-                "X-Csrf-Token": self.csrf_token,
-            },
-            cookies={"JSESSIONID": self.session_id},
-        )
-
     def _get_slots(self, id: int) -> list[tuple[datetime, int]]:
-        response = self._post(
+        response = self.session.post(
             PASSAPORTOONLINE_URL.format("n/rc/v1/utility/elenca-agenda-appuntamenti-sede-mese"),
             json={"sede": {"id": id}},
         )
@@ -71,7 +61,7 @@ class ApiScraper(IScraper):
         return entries
 
     def _scrape_availability(self) -> list[AvailabilityEntry]:
-        response = self._post(
+        response = self.session.post(
             PASSAPORTOONLINE_URL.format("a/rc/v1/appuntamento/elenca-sede-prima-disponibilita"),
             json={"comune": {"provinciaQuestura": self.province}},
         )
